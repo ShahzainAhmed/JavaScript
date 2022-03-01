42689135242795a64cbb9edf05ddfba7300b60d1
ShahzainAhmed <shahzainahmed57@gmail.com>
2022-03-01 22:22:42 +0500

    commit

---
+++ Switch Statement/script.js
@@ -0,0 +1,18 @@
+var fruit = "Apple";
+
+switch (fruit) {
+    case "Apple":
+        document.write("Apple is $2");
+        break;
+
+    case "Orange":
+        document.write("Orange is $4");
+        break;
+
+    case "Banana":
+        document.write("Banana is $6");
+        break;
+
+    default:
+        document.write("Market is closed today due to strike")
+}
