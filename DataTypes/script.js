97cf4e6347e85ef52c8131864b1eea8fe3f7b0ac
ShahzainAhmed <shahzainahmed57@gmail.com>
2022-03-01 20:09:04 +0500

    commit

---
+++ DataTypes/script.js
@@ -0,0 +1,15 @@
+alert("Welcome to DataTypes by Shahzain Ahmed!");
+
+var a = 10;
+var b = 76;
+var c = 0;
+var result = a + b;
+
+if (result == 86) {
+    document.write("Shahzain Ahmed is verified!");
+} else {
+    document.write("Shahzain is not verified.");
+}
+
+document.write(" " + Boolean(c));
+document.write(" " + Boolean(a));
