46c0cfe0eddec34a2457ad64d7c5547344db12e7
ShahzainAhmed <shahzainahmed57@gmail.com>
2022-03-01 23:02:43 +0500

    commit

---
+++ Loop/script.js
@@ -0,0 +1,27 @@
+var count = 0;
+
+// While loop 
+while (count < 10) {
+    document.write(count + " " + "While Loop by Shahzain Ahmed", "<br>");
+    count++; // count = count + 1
+}
+
+document.write("<br>"); // line break
+
+// Do while loop
+do {
+    document.write(count + " " + "Do While Loop by Shahzain Ahmed", "<br>");
+    count++;
+} while (count < 15);
+
+document.write("<br>"); // line break
+
+// for loop
+for (var newCount = 5; newCount < 10; newCount++) {
+    document.write(newCount + " " + "For Loop by Shahzain Ahmed", "<br>");
+}
+
+// for in loop (not discussed properly yet)
+// for (var forin in object) {
+
+// }
